ae94440096a16d5c4cf2e3f13d6f6bb2ed172176
Chris Tucker <chrisgtucker@hotmail.com>
2022-02-24 11:12:50 -0800

    commit of MoveMultpleFiles.ps1

---
+++ MoveMultipleFiles.ps1
@@ -0,0 +1,8 @@
+﻿# create an array of files. Enter in string as location. Can be relative to current working directory
+$files = @('', '', '')
+
+# Move each file in the array to the desired location 
+ForEach-Object{
+    Move-Item $files -Destination '' # Enter in string as desired location for file(s) to be moved to.
+    }
+
